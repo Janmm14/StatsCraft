90d5bc4f1fc30ac575d72cef1fe6994fb77cd77f
SirFaizdat <fdatoo7@gmail.com>
2016-08-11 19:48:15 -0400

    Initial commit

---
+++ src/main/java/org/statscraft/StatscraftApplication.java
@@ -0,0 +1,12 @@
+package org.statscraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatscraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatscraftApplication.class, args);
+	}
+}
---
+++ src/test/java/org/statscraft/StatscraftApplicationTests.java
@@ -0,0 +1,16 @@
+package org.statscraft;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StatscraftApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
